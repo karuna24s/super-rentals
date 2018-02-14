c23e303c2feb820f0cbee71fbe37823d2ec0bafe
karuna24s <karuna24s@gmail.com>
2018-02-14 14:38:31 -0500

    create a contact route and successfully implemented a test for it

--- tests/acceptance/list-rentals-test.js
+++ tests/acceptance/list-rentals-test.js
@@ -15,6 +15,11 @@ test('should link to information about the company.', function (assert) {
 });
 
 test('should link to contact information.', function (assert) {
+  visit('/');
+  click('a:contains("Contact")');
+  andThen(function() {
+    assert.equal(currentURL(), '/contact', 'should navigate to contact');
+  });
 });
 
 test('should list available rentals.', function (assert) {
